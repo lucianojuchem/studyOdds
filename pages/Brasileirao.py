8572f2902b4f02ff6b0e91a315502d8976e975f1
lucianojuchem <lucianojuchem@gmail.com>
2025-02-28 09:52:37 -0300

    ajuste

--- pages/Brasileirao.py
+++ pages/Brasileirao.py
@@ -2,7 +2,7 @@ import pandas as pd
 import gspread as gs
 import streamlit as st
 
-gc = gs.service_account(filename='FILENAME')
+gc = gs.service_account(filename='soccerdatabase-452117-c7df3916c9a9.json')
 
 dados = gc.open_by_url("https://docs.google.com/spreadsheets/d/1LyRKhG6ofCX20SEskr4mb00rw0nl38NbQvG_h3aL_0U/edit?gid=1735547126#gid=1735547126").worksheet("BRA")
 
